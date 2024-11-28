603ea874272e6931bcba510e37757399588d046e
henry-cv <thechosenone.cc@gmail.com>
2024-11-28 13:51:53 -0500

    tarea c11s. app.jsx modificado 0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Form } from "react-router-dom";
+import {Routes, Route, Form } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Home";
 import Contacto from "./Contacto";
 
 function App() {
   return (
-    <BrowserRouter>
+      <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/contacto" element={<Contacto />}></Route>
@@ -14,7 +14,7 @@ function App() {
       <h1>Mas que solo bebidas, festejemos el encuentro.</h1>
       <Navbar />
       <Home />
-    </BrowserRouter>
+      </>
   );
 }
 
