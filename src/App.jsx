11090d4ead7f9fc6fca53cf61b18a9c19a66d4c8
henry-cv <thechosenone.cc@gmail.com>
2024-11-27 21:31:08 -0500

    fe3 c11s cambio en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Form } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Home";
+import Contacto from "./Contacto";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contacto" element={<Contacto />}></Route>
+        <Route path="/beer/:id"></Route>
       </Routes>
       <h1>Mas que solo bebidas, festejemos el encuentro.</h1>
       <Navbar />
